7bdf3cfbf2a639dcbc691068147eea19545b8e66
guru-sai-shreesh <guru.sai.shreesh@gmail.com>
2021-03-03 17:11:55 +0530

    Initial Commit

---
+++ proj_LibraryManagementApp/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  proj_LibraryManagementApp
+//
+//  Created by Guru sai shreesh Tirumalla on 03/03/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
